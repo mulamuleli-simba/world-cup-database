d3022384bf4376eaa0c4faaf3b719abd6c73a4ac
mulamuleli-simba <65784695+mulamuleli-simba@users.noreply.github.com>
2024-03-15 01:06:34 +0200

    added files

---
+++ world_cuo.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE worldcup;
+\c worldcup;
+
+CREATE TABLE teams(team_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR UNIQUE NOT NULL);
+
+
+CREATE TABLE games(game_id SERIAL PRIMARY KEY NOT NULL, 
+    year INT NOT NULL,
+    round VARCHAR NOT NULL, 
+    winner_goals INT NOT NULL, 
+    opponent_goals INT NOT NULL,
+    winner_id INT NOT NULL,
+    FOREIGN KEY(winner_id) REFERENCES teams(team_id),
+    opponent_id INT NOT NULL,
+    FOREIGN KEY(opponent_id) REFERENCES teams(team_id));
+
